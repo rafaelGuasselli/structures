0a4bd5c223ba8a98199bd1bf08b54b700d7975cf
Kauan Fank <kauan.fank@gmail.com>
2025-03-25 20:42:21 -0300

    fast_pow

---
+++ math/fast_pow.cpp
@@ -0,0 +1,13 @@
+#define md 1000000007
+int fast_pow(int a, int b){ // Same logic (and almost the same code) can be applied 
+							// to all comutative and associative operations, sum, gcd, etc...
+ 	int res = 1;
+ 	while(b){
+ 		if(b & 1){
+ 			res = (a*b)%md;
+ 		}
+ 		a = (a*a)%md;
+ 		b >>=1;
+ 	}
+ 	return res;
+ }
