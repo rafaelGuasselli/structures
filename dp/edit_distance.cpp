f64f22cab04146e3ad937b461808bdc939eb8283
Kauan Fank <kauan.fank@gmail.com>
2025-04-07 17:02:10 -0300

    dp e kmp

---
+++ dp/edit_distance.cpp
@@ -0,0 +1,12 @@
+string s, t;
+int edit(int s1, int t1, vector<vector<int>> &memo){
+	if(!t1) return s1;
+	if(!s1) return t1;
+	if(memo[s1][t1] != -1){
+		return memo[s1][t1];
+	}
+	if(s[s1-1] == t[t1-1]){
+		return memo[s1][t1] = edit(s1-1, t1-1, memo);
+	}
+	return memo[s1][t1] = 1 + min(min(edit(s1, t1-1, memo), edit(s1-1, t1, memo)), edit(s1-1, t1-1, memo));
+}
---
+++ strings/kmp.cpp
@@ -0,0 +1,21 @@
+int kmp(string &t, string &p){
+	vector<int> lps = prefix_function(t);
+	int i = 0, j = 0;
+	vector<int> res;
+	while (i < t.size()) {
+		if (t[i] == p[j]) {
+			i++, j++;
+			if (j == p.size()) {
+				res.push_back(i-j);
+				j = lps[j-1];
+			}
+		} else {
+			if (j != 0) {
+				j = lps[j-1];
+			} else {
+				i++;
+			}
+		}
+	}
+	return pos;
+}
