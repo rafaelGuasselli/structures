bfe3af8b86a378c31aed65be4bface35b9499154
Luam Alves de Oliveira <luamoliveira.d@gmail.com>
2025-05-10 23:57:17 -0300

    dp edit_distance recursive to iterative

--- dp/edit_distance.cpp
+++ dp/edit_distance.cpp
@@ -1,13 +1,26 @@
 #include "../base.cpp"
-string s, t;
-int edit(int s1, int t1, vector<vector<int>> &memo){
-	if(!t1) return s1;
-	if(!s1) return t1;
-	if(memo[s1][t1] != -1){
-		return memo[s1][t1];
+
+int editDistance(string a, string b) {
+	if (a.size() > b.size()) swap(a,b);
+	int n = a.size(), m =b.size();
+
+	vector<vector<int>> dp_(n+1, vector<int>(m+1, 0));
+	for(int i=0;i<=n;i++) {
+		dp_[i][0] = i;
 	}
-	if(s[s1-1] == t[t1-1]){
-		return memo[s1][t1] = edit(s1-1, t1-1, memo);
+	for(int i=0;i<=m;i++) {
+		dp_[0][i] = i;
 	}
-	return memo[s1][t1] = 1 + min(min(edit(s1, t1-1, memo), edit(s1-1, t1, memo)), edit(s1-1, t1-1, memo));
+	
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=m;j++) {
+			if (a[i-1] == b[j-1]) {
+				dp_[i][j] = dp_[i-1][j-1];
+			} else {
+				dp_[i][j] = min({dp_[i-1][j], dp_[i][j-1], dp_[i-1][j-1]}) + 1;
+			}
+		}
+	}
+
+	return dp_[n][m];
 }
