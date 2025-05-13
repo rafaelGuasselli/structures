a72067cdcdaf025de04025714ba57f1383b008b8
Luam Alves de Oliveira <luamoliveira.d@gmail.com>
2025-05-12 22:37:30 -0300

    dp tsp

---
+++ dp/tsp.cpp
@@ -0,0 +1,34 @@
+#include "../base.cpp"
+
+const int MAXN = 20;
+int dp[MAXN][1<<MAXN], cost[MAXN][MAXN];
+
+// Is necessary change too many things during the contest
+// but, its a good base for the problem
+int tsp (int n) {
+    memset(dp, 0x3f, sizeof(dp));
+
+	for(int i=0;i<n-1;i++) {
+		dp[i][1 << i] = cost[0][i+1];
+	}
+
+	for(int i=1;i<1<<(n-1);i++){
+		for(int j=0;j<n-1;j++) {
+			if (!(i & (1 << j))) continue; // not visited yet
+			int value = dp[j][i];
+			int u = j + 1;
+			for(int k =0;k<n-1;k++) {
+				if ((i & (1 << k))) continue; // already visited
+				int newmask = i|(1 << k);
+				dp[k][newmask] = min(dp[k][newmask], value + cost[u][k+1]);
+		  	}
+		}
+	}
+	// INF
+	int ans = INF;
+	for(int i = 0;i<(n-1);i++){
+		ans = min(ans, dp[i][(1<<(n-1))-1] + cost[i+1][0]);
+	}
+
+	return ans;
+}
---
+++ tests/bustour.cpp
@@ -0,0 +1,86 @@
+#include "../base.cpp"
+
+// QUESTION KATTIS bustour - Bus Tour
+// TESTING: DP -> tsp
+
+
+int dp1[18][1000000], dp2[18][1000000];
+int adj[30][30];
+int n,m;
+
+// ideia: fazer dois TSP um saindo de 0 e outro saindo de n
+// depois comparar os caminhos ja visitados no meio do caminho
+// encontra a menor soma saindo da esquerda e chegando num no v
+// outro que comece em v e chegue no fim a mesma coisa para fim - inicio
+
+int main() {
+    int t = 0;
+    while(cin>>n>>m) {
+        memset(adj, 0x3f, sizeof(adj));
+        for(int i=0;i<m;i++) {
+            int a,b,c;cin>>a>>b>>c;
+            adj[a][b] = adj[b][a] = c;
+        }
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if (i == j) continue;   
+                for(int k =0;k<n;k++) {
+                    adj[i][j] = adj[j][i] = min(adj[i][j], adj[i][k] + adj[k][j]); 
+                }
+            }
+        }
+
+        memset(dp1, 0x3f, sizeof(dp1));
+        memset(dp2, 0x3f, sizeof(dp2));
+        int pot = 1;
+        for(int i=1;pot < n-1;pot++, i*=2) {
+            dp1[pot-1][i] = adj[0][pot];
+            dp2[pot-1][i] = adj[n-1][pot];
+        }
+
+        for(int i = 1;i<(1<<(n-2));i++) {
+            for(int j = 0;j<n-2;j++) {
+                if (!(i & (1<<j))) continue;
+                for(int k=0;k<n-2;k++) {
+                    if (i & (1 << k)) continue;
+                    dp1[k][i|(1<<k)] = min(dp1[j][i] + adj[k+1][j+1], dp1[k][i|(1<<k)]);
+                    dp2[k][i|(1<<k)] = min(dp2[j][i] + adj[k+1][j+1], dp2[k][i|(1<<k)]);
+                }
+            }
+        }
+
+        vector<int> comb(n-2);
+        int hh = (n-1)/2;
+        for(int i=0;i<hh;i++) {
+            comb[i] = 1;
+        }
+        reverse(comb.begin(), comb.end());
+        int ans = 1e9;
+		// If exists just one hotel
+        if (hh == n-2) ans = (adj[0][1] + adj[2][1]) * 2;
+        do {
+            int a1 = 1e9,a2 = 1e9;
+            int numb = 0;
+            int inv = 0;
+
+            for(int i=0;i<n-2;i++) {
+                numb += comb[i] << i;
+                inv += (comb[i] == 0)<<i;
+            }
+            for(int j=0;j<n-2;j++) {
+                if (!(numb & (1 << j))) continue;
+                for(int k=0;k<n-2;k++) {
+                    if (!(inv & (1 << k))) continue;
+                    a1 = min(dp1[j][numb] + dp2[k][inv] + adj[j+1][k+1], a1);
+                    a2 = min(dp2[j][numb] + dp1[k][inv] + adj[j+1][k+1], a2);
+                    ans = min(ans, a1 + a2);
+                }
+            }
+        } while(next_permutation(comb.begin(), comb.end()));
+
+        cout <<"Case "<<++t<<": "<< ans << endl;
+
+    }
+    return 0;
+}
