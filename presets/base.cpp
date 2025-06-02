bf53a77cd0955083978396f75df10fed9cf2ef82
Kauan Fank <kauan.fank@gmail.com>
2025-06-02 16:01:16 -0300

    Adicionei rng forte no base.cpp

--- presets/base.cpp
+++ presets/base.cpp
@@ -12,5 +12,12 @@ void fast_io(){
 }
 
 int main(){
-  
+	/*	STRONG RANDOM GENERATION
+
+		random_device rd;
+		mt19937 rng(rd());
+		uniform_int_distribution<int> dist(1, 3); // 1, 3 incluse range
+		dist(rng) -> returns random number between 1 and 3 with equal chance distribution.
+
+	*/
 }
