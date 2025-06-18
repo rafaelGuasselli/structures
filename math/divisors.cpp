0e6c28932ef5d2abe0e792f6d2c153a7ce897820
Kauan Fank <kauan.fank@gmail.com>
2025-06-17 23:03:47 -0300

    Modifiquei o divisors por uma versão bem melhor que descobri

--- math/divisors.cpp
+++ math/divisors.cpp
@@ -1,19 +1,14 @@
 #include "../presets/base.cpp"
 // Get all divisors via a map with the prime factors
 // Probably could be done better.
-void divisors(map<int, int> &factors, set<int> &d){
-  d.clear(); d.insert(1);
-  for(auto e: factors){
-    int p = e.first;
-    int exp = e.second;
-    vector<int> aux;
-    for(auto c : d){
-      long long temp = 1;
-      for(int i = 0; i < exp; i++){
-        temp *= p;
-        aux.push_back(c * temp);
-      }
-    }
-    for(auto v : aux) d.insert(v);
-  }
+set<int> divisors(int n){
+	set<int> d;
+	for(int i = 1; i*i <=n ; i++){
+		if(n % i == 0) d.insert(i);
+	}
+	vector<int> v;
+	for(int e: d){
+		v.push_back(n/e);
+	}
+	for(int e: v) d.insert(e);
 }
