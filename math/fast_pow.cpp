5e0550cf35fdd27b8b1c4fd668b1df4d8580dd66
Kauan Elias Schneider Fank <141959008+Itaxo01@users.noreply.github.com>
2025-07-09 19:03:31 -0300

    Corrigido o erro do fast_pow

--- math/fast_pow.cpp
+++ math/fast_pow.cpp
@@ -1,11 +1,10 @@
 #include "../presets/base.cpp"
 #define md 1000000007
-int fast_pow(int a, int b){ // Same logic (and almost the same code) can be applied 
-              // to all comutative and associative operations, sum, gcd, etc...
+int fast_pow(int a, int b){ 
    int res = 1;
    while(b){
      if(b & 1){
-       res = (a*b)%md;
+       res = (res*a)%md;
      }
      a = (a*a)%md;
      b >>=1;
