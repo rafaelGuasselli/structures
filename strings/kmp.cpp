71d37ef6721334785c10f73745a144f8e27041b1
Kauan Elias Schneider Fank <141959008+Itaxo01@users.noreply.github.com>
2025-07-29 14:23:41 -0300

    Update kmp.cpp

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -1,5 +1,21 @@
 #include "../presets/base.cpp"
-#include "prefix_function.cpp"
+
+// Tem alguma coisa errada aqui
+
+vector<int> prefix_function(string &s) {
+  int n = (int)s.length();
+  vector<int> pi(n);
+  for (int i = 1; i < n; i++) {
+    int j = pi[i-1];
+    while (j > 0 && s[i] != s[j])
+      j = pi[j-1];
+    if (s[i] == s[j])
+      j++;
+    pi[i] = j;
+  }
+  return pi;
+}
+
 vector<int> kmp(string &t, string &p){
   vector<int> lps = prefix_function(t);
   int i = 0, j = 0;
